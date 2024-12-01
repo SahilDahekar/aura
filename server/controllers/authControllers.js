bf3fe5f31e9b3386887501fdb0fa0231a8e9bc56
Parth Bhattad <bhattadparth20@gmail.com>
2024-12-01 21:48:05 +0530

    done

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -21,7 +21,7 @@ export const register = async (req, res) => {
         res.cookie("token", token, {
             path: "/",
             httpOnly: true,
-            domain: "localhost",
+            domain: "parthbhattad.in",
             maxAge: 60 * 60 * 1000,
             sameSite: "lax",
         });
@@ -68,7 +68,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             path: "/",
             httpOnly: true,
-            domain: "localhost",
+            domain: "parthbhattad.in",
             maxAge: 60 * 60 * 1000,
             sameSite: "lax",
         });
@@ -97,7 +97,7 @@ export const logout = async (req, res, next) => {
 
         res.clearCookie("token", {
             httpOnly: true,
-            domain: "localhost",
+            domain: "Parthbhattad.in",
             signed: true,
             path: "/",
         });
